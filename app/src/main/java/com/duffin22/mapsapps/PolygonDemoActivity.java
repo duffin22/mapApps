92673ee350665c2ac30ccfafe57369b2a45ba8c5
Matthew Duffin <matthewtduffin@gmail.com>
2016-09-02 14:30:23 -0700

    plotting a translucent square in sydney

--- app/src/main/java/com/duffin22/mapsapps/PolygonDemoActivity.java
+++ app/src/main/java/com/duffin22/mapsapps/PolygonDemoActivity.java
@@ -1,8 +1,11 @@
 package com.duffin22.mapsapps;
 
+import android.graphics.Color;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.CheckBox;
 import android.widget.SeekBar;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -11,44 +14,77 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class PolygonDemoActivity extends AppCompatActivity
-            implements SeekBar.OnSeekBarChangeListener, OnMapReadyCallback {
+            implements OnMapReadyCallback {
+
+    private static final LatLng SYDNEY = new LatLng(-33.87365, 151.20689);
+
+    private static final int WIDTH_MAX = 50;
+
+    private static final int HUE_MAX = 360;
+
+    private static final int ALPHA_MAX = 255;
+
+    private Polygon mMutablePolygon;
+
+    private Polygon mClickablePolygonWithHoles;
+
+    private SeekBar mColorBar;
+
+    private SeekBar mAlphaBar;
+
+    private SeekBar mWidthBar;
+
+    private CheckBox mClickabilityCheckbox;
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+        setContentView(R.layout.polygon_demo);
+
+        SupportMapFragment mapFragment =
+                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
     }
 
     @Override
-    public void onMapReady(GoogleMap googleMap) {
-        mMap = googleMap;
+    public void onMapReady(GoogleMap map) {
+        // Override the default content description on the view, for accessibility mode.
+        // Ideally this string would be localised.
+        map.setContentDescription("Google Map with polygons.");
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        map.addPolygon(new PolygonOptions()
+                .addAll(createRectangle(new LatLng(-27, 140), 10, 7))
+                .fillColor(Color.argb(90,112,123,43))
+                .strokeColor(Color.BLACK));
+
+        // Move the map so that it is centered on the mutable polygon.
+        map.moveCamera(CameraUpdateFactory.newLatLng(SYDNEY));
+
+        // Add a listener for polygon clicks that changes the clicked polygon's stroke color.
+        map.setOnPolygonClickListener(new GoogleMap.OnPolygonClickListener() {
+            @Override
+            public void onPolygonClick(Polygon polygon) {
+                // Flip the r, g and b components of the polygon's stroke color.
+                int strokeColor = polygon.getStrokeColor() ^ 0x00ffffff;
+                polygon.setStrokeColor(strokeColor);
+            }
+        });
     }
 
-    @Override
-    public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-
-    }
-
-    @Override
-    public void onStartTrackingTouch(SeekBar seekBar) {
-
-    }
-
-    @Override
-    public void onStopTrackingTouch(SeekBar seekBar) {
-
+    private List<LatLng> createRectangle(LatLng center, double halfWidth, double halfHeight) {
+        return Arrays.asList(new LatLng(center.latitude - halfHeight, center.longitude - halfWidth),
+                new LatLng(center.latitude - halfHeight, center.longitude + halfWidth),
+                new LatLng(center.latitude + halfHeight, center.longitude + halfWidth),
+                new LatLng(center.latitude + halfHeight, center.longitude - halfWidth),
+                new LatLng(center.latitude - halfHeight, center.longitude - halfWidth));
     }
 }
